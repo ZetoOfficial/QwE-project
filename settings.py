3cd64ca124fa7e59b9be9c5e70c3595f0ebfd65e
ZetoQqq <tpptgs@bk.ru>
2022-04-23 18:03:29 +0500

    Добавил логгирование

--- settings.py
+++ settings.py
@@ -1,7 +1,15 @@
+from pathlib import Path
 from yaml import load, SafeLoader
 from pydantic import BaseSettings, BaseModel
+from logging import basicConfig, getLogger, FileHandler, StreamHandler, INFO, DEBUG
 
-CONFIG_FILE = "settings.yaml"
+CONFIG_FILE = str(Path(__file__).parent.absolute()) + "/settings.yaml"
+LOGFILE_FILE = str(Path(__file__).parent.absolute()) + "/qwe.log"
+basicConfig(
+    level=INFO,
+    format="[%(asctime)s] [%(levelname)s] [%(name)s] [%(funcName)s():%(lineno)s] %(message)s",
+    handlers=[FileHandler(LOGFILE_FILE), StreamHandler()],
+)
 with open(CONFIG_FILE, "r") as f:
     cfg = load(f, SafeLoader)
 
