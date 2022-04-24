5815d4c7710e11718e833ea0e1391018ae45370d
ZetoQqq <tpptgs@bk.ru>
2022-04-24 14:45:04 +0500

    Добавил CORSMiddleware

--- app/service.py
+++ app/service.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.crud import get_vacancies
 from app.schemas import VacancyDB
 from app.models import Base
@@ -7,6 +9,23 @@ from app.database import engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://utmn.su",
+    "https://utmn.su",
+    "http://api.utmn.su",
+    "https://api.utmn.su",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/vacancies/", response_model=list[VacancyDB])
